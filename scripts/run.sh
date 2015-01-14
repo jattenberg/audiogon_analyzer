8d51cdd904df1212a6e6ee61ed0acdf3f5a8c2fd
jattenberg <jattenberg@etsy.com>
2015-01-14 12:03:04 +0000

    updating

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,3 +1,6 @@
-python /home/jattenberg/development/audiogon_analyzer/scripts/gather.py >> /home/jattenberg/development/audiogon_analyzer/data/output.log
+cd /home/jattenberg/development/audiogon_analyzer/
+
+/home/jattenberg/anaconda/bin/python /home/jattenberg/development/audiogon_analyzer/scripts/gather.py >> /home/jattenberg/development/audiogon_analyzer/data/output.log
+
 git commit -a -m "updating"
 git rpull && git push
